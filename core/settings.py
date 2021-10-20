ded1fd4966b70a2d4195522a117beececbefd77b
elkarouani <elkarouani@gmail.com>
2021-10-20 07:47:56 +0100

    Task: create users app

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    
+    # Local
+    'store.apps.StoreConfig',
+    'users.apps.UsersConfig',
 ]
+
+AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
