58a6217ed1e88652994545aa01c1dbc23d95edea
elkarouani <elkarouani@gmail.com>
2021-12-02 21:57:25 +0100

    Task: adding checkout form section

--- app/pages/checkout/index.tsx
+++ app/pages/checkout/index.tsx
@@ -1,10 +1,11 @@
-import { Header } from 'sections';
+import { CheckoutForm, Header } from 'sections';
 
 export default function Checkout() {
   return (
     <div className="flex flex-col">
       <Header without_catergories_dropdown />
-      <div className="py-5 flex justify-center">
+      <CheckoutForm />
+      <div className="mt-12 py-5 flex justify-center">
         <button className="CartCallToAction">
           Pay
         </button>
