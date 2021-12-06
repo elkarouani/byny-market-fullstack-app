1094f6251467ff169f3a144db89adcf6316520e7
elkarouani <elkarouani@gmail.com>
2021-12-06 19:29:22 +0100

    Task: link related components to auth page

--- app/sections/Auth/SocialAuthActions/SocialAuthActions.tsx
+++ app/sections/Auth/SocialAuthActions/SocialAuthActions.tsx
@@ -2,22 +2,22 @@ import { GoogleAuth, LinkedinAuth, TwitterAuth } from "components/UI";
 
 const SocialAuthActions = () => {
 	return (
-		<div className="flex space-x-16">
-			<div 
+		<div className="flex space-x-16 mt-6">
+			<div
 				id="google_social_auth"
 				className="p-2 border rounded-xl"
 			>
 				<GoogleAuth />
 			</div>
 
-			<div 
+			<div
 				id="twitter_social_auth"
 				className="p-2 border rounded-xl"
 			>
 				<TwitterAuth />
 			</div>
 
-			<div 
+			<div
 				id="linkedin_social_auth"
 				className="p-2 border rounded-xl"
 			>
